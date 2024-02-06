6b3bab8349eb1c8c7850def40203ca4cdbdcd583
duokhongg3937 <nosurprise39@gmail.com>
2024-02-06 09:45:15 +0700

    Add CORS support

--- API/Program.cs
+++ API/Program.cs
@@ -17,6 +17,7 @@ app.UseStatusCodePagesWithReExecute("/errors/{0}");
 app.UseSwagger();
 app.UseSwaggerUI();
 app.UseStaticFiles();
+app.UseCors("CorsPolicy");
 app.UseAuthorization();
 app.MapControllers();
 #endregion
