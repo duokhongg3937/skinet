dbf7da093dd53b5f9b510bab0be276afb06f8d7e
duokhongg3937 <nosurprise39@gmail.com>
2024-02-07 19:23:46 +0700

    Add navbar component

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'client';
+export class AppComponent implements OnInit {
+  title = 'Skinet Client';
+  products: any[] = [];
+
+  constructor(private http: HttpClient) {
+
+  }
+  ngOnInit(): void {
+    this.http.get("https://localhost:5001/api/products").subscribe({
+      next: (response: any) => this.products = response.data,
+      error: error => console.log(error),
+      complete: () => {
+        console.log("Request Completed");
+        console.log("Extra Statement");
+      }
+    })
+  }
 }
