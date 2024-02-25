2b895d852298180e780371f39c49808a52e3e247
duokhongg3937 <nosurprise39@gmail.com>
2024-02-25 22:30:42 +0700

    Add identity in client

---
+++ client/src/app/account/register/register.component.ts
@@ -0,0 +1,48 @@
+import { Component } from '@angular/core';
+import { AbstractControl, AsyncValidatorFn, FormBuilder, Validators } from '@angular/forms';
+import { AccountService } from '../account.service';
+import { Router } from '@angular/router';
+import { debounceTime, finalize, map, switchMap, take } from 'rxjs';
+
+@Component({
+  selector: 'app-register',
+  templateUrl: './register.component.html',
+  styleUrls: ['./register.component.scss']
+})
+export class RegisterComponent {
+  errors: string[] | null = null;
+
+  constructor(private formBuilder: FormBuilder, private accountService: AccountService,
+    private router: Router) { }
+
+  complexPassword = "(?=^.{6,10}$)(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%^&amp;*()_+}{&quot;:;'?/&gt;.&lt;,])(?!.*\s).*$";
+
+  registerForm = this.formBuilder.group({
+    displayName: ['', Validators.required],
+    email: ['', [Validators.required, Validators.email], [this.validateEmailNotTaken()]],
+    password: ['', [Validators.required, Validators.pattern(this.complexPassword)]]
+  })
+
+  onSubmit() {
+    this.accountService.register(this.registerForm.value).subscribe({
+      next: () => this.router.navigateByUrl("/shop"),
+      error: err => this.errors = err.errors
+    })
+  }
+
+  validateEmailNotTaken(): AsyncValidatorFn {
+    return (control: AbstractControl) => {
+      return control.valueChanges.pipe(
+        debounceTime(500),
+        take(1),
+        switchMap(() => {
+          return this.accountService.checkEmailExists(control.value).pipe(
+            map(result => result ? { emailExists: true } : null),
+            finalize(() => control.markAsTouched())
+          )
+        })
+      )
+    }
+  }
+
+}
