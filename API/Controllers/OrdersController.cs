7ecb5d81de3ea1b6ef947714517e4b472085ef6c
duokhongg3937 <nosurprise39@gmail.com>
2024-02-28 21:31:21 +0700

    Refactor order service using UnitOfWork

--- API/Controllers/OrdersController.cs
+++ API/Controllers/OrdersController.cs
@@ -31,5 +31,31 @@ namespace API.Controllers
             if (order == null) return BadRequest(new ApiResponse(400, "Problem creating order"));
             return Ok(order);
         }
+
+        [HttpGet]
+        public async Task<ActionResult<IReadOnlyList<OrderToReturnDto>>> GetOrdersForUser()
+        {
+            var email = HttpContext.User.RetrieveEmailFromPrincipal();
+            var orders = await _orderService.GetOrdersForUserAsync(email);
+
+            return Ok(_mapper.Map<IReadOnlyList<OrderToReturnDto>>(orders));
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<OrderToReturnDto>> GetOrderByIdForUser(int id)
+        {
+            var email = HttpContext.User.RetrieveEmailFromPrincipal();
+            var order = await _orderService.GetOrderByIdAsync(id, email);
+
+            if (order == null) return NotFound(new ApiResponse(404));
+            return Ok(_mapper.Map<OrderToReturnDto>(order));
+        }
+
+        [HttpGet("deliveryMethods")]
+        public async Task<ActionResult<IReadOnlyList<DeliveryMethod>>> GetDeliveryMethods()
+        {
+            var methods = await _orderService.GetDeliveryMethodsAsync();
+            return Ok(methods);
+        }
     }
 }
